c801723f730dc7aff9b64679a839c4e29147289a
Web developer with a thing for ReactJs <suhasdwarakanath@gmail.com>
2019-06-13 12:13:00 -0700

    Updated

--- Frontend/typeahead-suggestion-delay.js
+++ Frontend/typeahead-suggestion-delay.js
@@ -11,3 +11,29 @@ function searchTimer() {
         makeAPICall(input.value)
     }, 400);
 }
+                               
+                       
+// Approach 2
+                               
+HTML:
+
+Search Query
+
+javascript
+
+var searchField = document.getElementById("searchField"),
+      lastTimeId,
+      doSearchFun = function(e) {
+
+      if(typeof lastTimeId !== 'undefined') {
+          clearTimeout(lastTimeId);
+      }
+
+      lastTimeId = setTimeout[function() {
+               // Do search stuff
+               console.log('Do search for "' + searchField.value + '"');
+          }, 400);
+     }
+;
+
+searchField.addEventListener("keydown", doSearchFun);
